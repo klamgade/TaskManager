16862c0d6419be2759a3ad8ed83a03f8bc6fab36
Kamal Lamgade <kamal.lamgade@n4l.co.nz>
2021-11-24 23:11:59 +1300

    test a circle ci build fail with test change

--- src/tests/unit-tests/sum.test.js
+++ src/tests/unit-tests/sum.test.js
@@ -3,4 +3,6 @@ const Calculate = require('../../routes/handlers/calculate')
 test("add 1 + 2 to equal 3", () => {
     let calculate = new Calculate();
     expect(calculate.sum(1,2)).toBe(3);
+    //just for test of circleci build fail
+    expect(calculate.sum(1,2)).toBe(4);
 })
